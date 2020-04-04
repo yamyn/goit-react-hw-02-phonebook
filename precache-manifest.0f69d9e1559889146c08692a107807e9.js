1bfc97253ba6b8ec8147a7cdba3311eb18d23511
Ya_Myn <ricky.month@gmail.com>
2020-04-04 12:40:23 +0300

    Updates

---
+++ precache-manifest.0f69d9e1559889146c08692a107807e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae83dc3fae13f1e34973716e8e4b72a8",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "6fb1e04d70ad81c9ec7a",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.fd94e982.chunk.css"
+  },
+  {
+    "revision": "c554c5de4c2f3bf9547e",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.4032be10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.4032be10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fb1e04d70ad81c9ec7a",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.8279c386.chunk.js"
+  },
+  {
+    "revision": "4dc4691f794833de7ba9",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.bd2cb211.js"
+  }
+]);
